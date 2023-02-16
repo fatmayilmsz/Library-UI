2e840a5be4d2e19460b17456636a3583528d8749
Fatma Yılmaz <79848362+fatmayilmsz@users.noreply.github.com>
2023-02-16 19:57:40 +0300

    Form validations
    update 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,21 +8,59 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 
 
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   //Create FormGroup
-  requiredForm!: FormGroup;
-  constructor(private fb: FormBuilder) {
-     this.myForm();
-  }
+//   loginForm!: FormGroup;
+//   constructor(private fb: FormBuilder) {
+//      this.myForm();
+//   }
 
-  myForm() {
-    this.requiredForm = this.fb.group({
-    name: ['', Validators.required ],
+//   myForm() {
+//     this.loginForm = this.fb.group({
+//       email: ['', Validators.required ],
+//     });
+//  }
+//  ngOnInit()
+//  {
 
-    });
- }
- ngOnInit()
- {
+//  }
 
- }
+title_login = 'GİRİŞ YAP';
+loginForm = new FormGroup({
+  lemail: new FormControl('',[Validators.required,Validators.email]),
+  lpassword: new FormControl('',[Validators.required])
+})
+loginUser(){
+  console.warn(this.loginForm.value)
 }
+get lemail(){
+  return this.loginForm.get('lemail');
+}
+get lpassword(){
+  return this.loginForm.get('lpassword');
+}
+title_signup = 'KAYIT OL';
+signupForm = new FormGroup({
+   sname: new FormControl('',[Validators.required]),
+   slastname: new FormControl('',[Validators.required]),
+   semail: new FormControl('',[Validators.required,Validators.email]),
+   spassword: new FormControl('',[Validators.required]),
+   spasswordagain: new FormControl('',[Validators.required]),
+   scontract: new FormControl(false,[Validators.requiredTrue])
+})
+get sname(){
+  return this.signupForm.get('sname');
+}
+get slastname(){
+  return this.signupForm.get('slastname');
+}get semail(){
+  return this.signupForm.get('semail');
+}
+get spassword(){
+  return this.signupForm.get('spassword');
+}get spasswordagain(){
+  return this.signupForm.get('spasswordagain');
+}
+get scontract(){
+  return this.signupForm.get('scontract');
+}}
