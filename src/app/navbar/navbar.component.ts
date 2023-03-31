26c65864eedb64665b57354fde8e4c2f88e98984
Fatma Yılmaz <79848362+fatmayilmsz@users.noreply.github.com>
2023-04-01 01:42:00 +0300

    css update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AuthService } from '../services/auth.service'; 
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, OperatorFunction, Subject, debounceTime, distinctUntilChanged, filter, map, merge } from 'rxjs';
+const states = [
+	'Romantik',
+	'Dram',
+	'Yeni Çıkanlar',
+	'Aksiyon',
+	'Macera',
+	'Ders Kitapları',
+	'Yazarlar',
+	'Kitaplar',
+	'Romanlar',
+	'Halit Ziya Uşaklıgil',
+	'Federated States Of Micronesia',
+	'Florida',
+	'Georgia',
+	'Guam',
+	'Hawaii',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Iowa',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Maine',
+	'Marshall Islands',
+	'Maryland',
+	'Massachusetts',
+	'Michigan',
+	'Minnesota',
+	'Mississippi',
+	'Missouri',
+	'Montana',
+	'Nebraska',
+	'Nevada',
+	'New Hampshire',
+	'New Jersey',
+	'New Mexico',
+	'New York',
+	'North Carolina',
+	'North Dakota',
+	'Northern Mariana Islands',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Palau',
+	'Pennsylvania',
+	'Puerto Rico',
+	'Rhode Island',
+	'South Carolina',
+	'South Dakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Vermont',
+	'Virgin Islands',
+	'Virginia',
+	'Washington',
+	'West Virginia',
+	'Wisconsin',
+	'Wyoming',
+];
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,6 +74,23 @@ export class NavbarComponent {
   constructor(private authService: AuthService) {}
   ngOnInit() {
   }
+	model: any;
+
+	@ViewChild('instance', { static: true }) instance!: NgbTypeahead;
+	focus$ = new Subject<string>();
+	click$ = new Subject<string>();
+
+	search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+		const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+		const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+		const inputFocus$ = this.focus$;
+
+		return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+			map((term) =>
+				(term === '' ? states : states.filter((v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10),
+			),
+		);
+	};
 
   getFullName():any{
     return localStorage.getItem("ns");
